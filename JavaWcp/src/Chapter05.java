763db8a782cc411db1ed5cd8684889b504df8555
core-melt <core.melt21@gmail.com>
2020-08-26 17:52:55 +0900

    [Update]Chapter05

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,22 @@
+
+public class Chapter05 {
+    public static void main(String[] args) {
+    	long l1 = 1;
+    	double d1 = 2.1d;
+    	boolean b1 = false;
+    	System.out.println(l1);
+    	System.out.println(d1);
+    	System.out.println(b1);
+
+    	Float f1 = 3.2f;
+    	Integer i1 = 4;
+    	Character c1 = '5';
+    	System.out.println(f1);
+    	System.out.println(i1);
+    	System.out.println(c1);
+
+    	int[] array1 = new int[] { 10, 20, 30, 40, 50 };
+    	System.out.println(array1[3]);
+
+    }
+}
