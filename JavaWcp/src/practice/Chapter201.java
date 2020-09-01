5ff5d865564103902bdb4e7732a46a27d57d2fc8
core-melt <core.melt21@gmail.com>
2020-09-01 11:47:38 +0900

    [Create]Chapter201

---
+++ JavaWcp/src/practice/Chapter201.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class Chapter201 {
+    public static void main(String[] args) {
+		Human man = new Human();
+		Human.Humanleg manleg = man.new Humanleg();
+		manleg.kick();
+	}
+}
